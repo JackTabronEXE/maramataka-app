9306315f2d0a5c78e0b50d8082604a17e099eabf
mere-mansell <102892063+mere-mansell@users.noreply.github.com>
2022-07-14 17:05:34 +1200

    add Dates component and finish front end

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Dates from './Dates'
 
-function App () {
+function App() {
   return (
     <>
       <header className="header">
         <h1>My Collection</h1>
       </header>
       <section className="main">
+        <Dates />
         {/* add your code here */}
       </section>
     </>
