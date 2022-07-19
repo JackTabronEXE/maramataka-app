8fa30cbd931f0e964ebcdd2606f57396291b227d
phoresced <62972420+phoresced@users.noreply.github.com>
2022-07-19 15:47:41 +1200

    React state to handle value change

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { format } from 'date-fns'
-import toDate from 'date-fns/toDate'
+import React, { useState } from 'react'
+// import { format } from 'date-fns'
+import { useDispatch } from 'react-redux'
 import styles from './Calendar.module.scss'
 
 export default function Calendar() {
-  const date = format(toDate(Date.now()), 'yyyy-MM-dd')
+  const dispatch = useDispatch()
+  const [targetDate, setTargetDate] = useState(
+    new Date().toISOString().split('T')[0]
+  )
 
   function handleChange(e) {
-    return e.preventDefault
+    e.preventDefault()
+    // const now = e.target.value
+    // const timestamp = now.toISOString()
+    setTargetDate(e.target.value)
+    console.log('Change react state to', e.target.value)
+  }
+
+  function changeMoon(e) {
+    e.preventDefault()
+    //dispatch(setDay())
+
+    // I can't use format on the datepicker return date (yyyy-mm-dd)
+    // I could convert the element into a string then return a substring for each part of the date and ship those?
+    // const date = format(targetDate, 'd MMMM y')
   }
 
   return (
@@ -19,12 +35,18 @@ export default function Calendar() {
         className={styles.dateinput}
         type="date"
         id="selectDate"
-        value={date}
+        defaultValue={targetDate}
         name="select-date"
         min="2022-07-14"
         max="2022-12-10"
+        // onFocus={() => {
+        //   console.log('Focus!')
+        // }}
         onChange={(e) => handleChange(e)}
       />
+      <button className={styles.datebutton} onClick={(e) => changeMoon(e)}>
+        Go
+      </button>
     </form>
   )
 }
--- client/components/Moon.jsx
+++ client/components/Moon.jsx
@@ -12,38 +12,45 @@ function Moon() {
   const today = datesList.find((day) => {
     return day.DMY == date
   })
-  console.log(today ? today.name : 'not yet')
 
   useEffect(() => {
     dispatch(fetchDates())
   }, [])
 
   // return <img src={`/images/moonphases/${datesList[0]?.image}`} alt="dummy" />
-  return(
-    <> 
- <section>
-  <div className="content">
-    <h2>{today?.name}</h2>
-    <h2>{today?.name}</h2>
-  </div>
-  <div className="date-text">
-    <h2> {today?.DMY.toUpperCase()} </h2>
-  </div>
-</section>
-    <div className="moon-text">
-    <h1> ENERGY: {today?.energy.toUpperCase()} </h1>
-    <p> {today?.goodFor} </p> 
-    <img className="moon-image" src={`/images/moonphases/${today?.image}`} alt="dummy" />
-    </div>
-    <a
-      className="arrow" onClick={() => {setTargetDate(addDays(targetDate, 1))
-      }}>
-    <div className="arrow">
-    <div className="arrow-top"></div>
-    <div className="arrow-bottom"></div>
-  </div>
-  </a>
-  </>
-  )}
- 
+  return (
+    <>
+      <section>
+        <div className="content">
+          <h2>{today?.name}</h2>
+          <h2>{today?.name}</h2>
+        </div>
+        <div className="date-text">
+          <h2> {today?.DMY.toUpperCase()} </h2>
+        </div>
+      </section>
+      <div className="moon-text">
+        <h1> ENERGY: {today?.energy.toUpperCase()} </h1>
+        <p> {today?.goodFor} </p>
+        <img
+          className="moon-image"
+          src={`/images/moonphases/${today?.image}`}
+          alt="dummy"
+        />
+      </div>
+      <a
+        className="arrow"
+        onClick={() => {
+          setTargetDate(addDays(targetDate, 1))
+        }}
+      >
+        <div className="arrow">
+          <div className="arrow-top"></div>
+          <div className="arrow-bottom"></div>
+        </div>
+      </a>
+    </>
+  )
+}
+
 export default Moon
