1f0e543b64736a6af374e66593075bdb46d448f6
phoresced <62972420+phoresced@users.noreply.github.com>
2022-07-19 16:50:48 +1200

    Fix bugs in reading the seed data

--- client/components/Moon.jsx
+++ client/components/Moon.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchDates } from '../actions'
-import { addDays } from 'date-fns'
+import { format, addDays } from 'date-fns'
 import toDate from 'date-fns/toDate'
 
 function Moon() {
@@ -11,51 +14,16 @@ function Moon() {
 
   const dispatch = useDispatch()
   const [targetDate, setTargetDate] = useState(toDate(Date.now()))
-  // const date = inputDay
+  const date = format(targetDate, 'd MMMM y')
   const today = datesList.find((day) => {
     return day.DMY == inputDay
   })
 
+  console.log('Calendar effect', inputDay, 'today:', today)
+  console.log(datesList)
   useEffect(() => {
     dispatch(fetchDates())
-    console.log('Moon gets data?', inputDay)
   }, [])
-
-  // return <img src={`/images/moonphases/${datesList[0]?.image}`} alt="dummy" />
-  //   return (
-  //     <>
-  //       <section>
-  //         <div className="content">
-  //           <h2>{today?.name}</h2>
-  //           <h2>{today?.name}</h2>
-  //         </div>
-  //         <div className="date-text">
-  //           <h2> {today?.DMY.toUpperCase()} </h2>
-  //         </div>
-  //       </section>
-  //       <div className="moon-text">
-  //         <h1> ENERGY: {today?.energy.toUpperCase()} </h1>
-  //         <p> {today?.goodFor} </p>
-  //         <img
-  //           className="moon-image"
-  //           src={`/images/moonphases/${today?.image}`}
-  //           alt="dummy"
-  //         />
-  //       </div>
-  //       <a
-  //         className="arrow"
-  //         onClick={() => {
-  //           setTargetDate(addDays(targetDate, 1))
-  //         }}
-  //       >
-  //         <div className="arrow">
-  //           <div className="arrow-top"></div>
-  //           <div className="arrow-bottom"></div>
-  //         </div>
-  //       </a>
-  //     </>
-  //   )
-  // }
 
   return (
     <>
