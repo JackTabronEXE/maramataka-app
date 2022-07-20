a3499bf7409338b1f6f22be821f1c0bf10f3ecc7
phoresced <62972420+phoresced@users.noreply.github.com>
2022-07-20 15:03:16 +1200

    Clean up for heroku deployment

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM, { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
